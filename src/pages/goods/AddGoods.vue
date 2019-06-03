6ccb81c461437b7580344377c72de01fe9b828e1
gitYubo <1142493958@qq.com>
2019-06-03 22:39:43 +0800

    0.0

--- src/pages/goods/AddGoods.vue
+++ src/pages/goods/AddGoods.vue
@@ -1,10 +1,65 @@
 <template>
-    <Card :dis-hover="true" :shadow="false" :bordered="false">
+    <Card :dis-hover="true" :shadow="false" :bordered="false" class="goods-add-content">
         <h4 slot="title">新建商品</h4>
-        <Steps :current="1">
-            <Step title="已完成" content="商品基本信息"></Step>
-            <Step title="进行中" content="商品详细信息"></Step>
+        <Steps :current="0">
+            <Step title="进行中" content="商品基本信息"></Step>
+            <Step title="进行中" content="商品详细信息"> </Step>
             <Step title="待进行" content="其他信息"></Step>
         </Steps>
+        <Tabs value="name1" class="tabs-nav-container">
+            <TabPane name="name1">
+                <AddGoods1/>
+            </TabPane>
+            <TabPane name="name2">
+                <AddGoods2/>
+            </TabPane>
+            <TabPane name="name3">
+                <AddGoods3/>
+            </TabPane>
+        </Tabs>
     </Card>
 </template>
+
+<script>
+import AddGoods1 from './components/AddGoods1'
+import AddGoods2 from './components/AddGoods1'
+import AddGoods3 from './components/AddGoods1'
+export default {
+    components:{
+        AddGoods1,
+        AddGoods2,
+        AddGoods3
+    },
+    data(){
+        return {
+            formValidate:{},
+            ruleValidate:{}
+        }
+    }
+}
+</script>
+
+
+<style lang="less" scoped>
+    .ivu-card-body{
+        padding: 16px 0;
+    }
+</style>
+<style>
+.goods-add-content{
+    
+}
+.tabs-nav-container .ivu-tabs-bar{
+    display: none;
+   
+  
+}
+.goods-add-content .ivu-card-body{
+    padding: 16px 0;
+}
+.ivu-steps{
+    margin-top: 20px;
+    margin-bottom: 35px;
+    margin-left: 100px;
+}
+</style>
---
+++ src/pages/goods/components/AddGoods1.vue
@@ -0,0 +1,97 @@
+<template>
+<div>
+    <Card :dis-hover="true" :shadow="false" :bordered="false" class="goods-add-item">
+        <h3 class="title">基本信息</h3>
+        <Form ref="formValidate" :model="formValidate" :rules="ruleValidate" :label-width="200">
+            <FormItem label="商品名称" prop="name">
+                <Input v-model="formValidate.name" placeholder="Enter your name"></Input>
+            </FormItem>
+            <FormItem label="商品类目" prop="mail">
+                <Button type="default" class="tools">选择商品类目</Button>
+            </FormItem>
+            <FormItem label="商品分组" prop="city">
+                <Select v-model="formValidate.city" placeholder="Select your city">
+                    <Option value="beijing">New York</Option>
+                    <Option value="shanghai">London</Option>
+                    <Option value="shenzhen">Sydney</Option>
+                </Select>
+            </FormItem>
+            <FormItem label="商品图片">
+                <Row>
+                    <Col span="11">
+                        <FormItem prop="date">
+                            <DatePicker type="date" placeholder="Select date" v-model="formValidate.date"></DatePicker>
+                        </FormItem>
+                    </Col>
+                    <Col span="2" style="text-align: center">-</Col>
+                    <Col span="11">
+                        <FormItem prop="time">
+                            <TimePicker type="time" placeholder="Select time" v-model="formValidate.time"></TimePicker>
+                        </FormItem>
+                    </Col>
+                </Row>
+                <div>尺寸建议750x750（正方形模式）或750×1000（长图模式）像素以上，大小2M以下，最多10张 (可拖拽图片调整显示顺序 )</div>
+            </FormItem>
+            <FormItem label="主图视频" prop="gender">
+                <RadioGroup v-model="formValidate.gender">
+                    <Radio label="male">Male</Radio>
+                    <Radio label="female">Female</Radio>
+                </RadioGroup>
+            </FormItem>
+        </Form>
+    </Card>
+    <Card :dis-hover="true" :shadow="false" :bordered="false" class="goods-add-item">
+        <h3 class="title">配送/价格设置</h3>
+        <Form ref="formValidate" :model="formValidate" :rules="ruleValidate" :label-width="200">
+            <FormItem label="配送方式" prop="name">
+                <Input v-model="formValidate.name" placeholder="Enter your name"></Input>
+            </FormItem>
+            <FormItem label="运费设置" prop="mail">
+                <Button type="default" class="tools">选择商品类目</Button>
+            </FormItem>
+
+            <FormItem label="商品规格" prop="gender">
+                <RadioGroup v-model="formValidate.gender">
+                    <Radio label="male">统一规格</Radio>
+                    <Radio label="female">多规格</Radio>
+                </RadioGroup>
+            </FormItem>
+
+            <FormItem>
+                <Button @click="handleReset('formValidate')"> 取消</Button>
+                <Button type="primary" @click="handleSubmit('formValidate')">下一步</Button>
+            </FormItem>
+        </Form>
+    </Card>
+</div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            formValidate:{},
+            ruleValidate:{}
+        }
+    }
+}
+</script>
+<style lang="less" scoped>
+.title{
+    padding-left: 16px;
+}
+</style>
+
+
+<style>
+.goods-add-item .ivu-tabs-nav{
+    display: none;
+}
+.goods-add-item .ivu-card-body{
+    background: #f2f2f6;
+    border-radius: 4px;
+    margin-bottom: 10px;
+}
+</style>
+
+
